84c2b6e2de4668232ce10424182bb6bbced5a935
Daniel Zawada <daniel.zawada@usask.ca>
2025-07-29 13:59:34 -0600

    swap licensing?

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,9 +1,8 @@
 SET PYTHONUTF8=1
 SET PYTHONIOENCODING=utf-8
 
+cd rust/sasktran2-py-ext/ && cargo-bundle-licenses --format yaml --output ../../THIRDPARTY.yml
 maturin build --release
 if %ERRORLEVEL% neq 0 exit %ERRORLEVEL%
 %PYTHON% -m pip install --find-links=rust\sasktran2-py-ext\target\wheels %PKG_NAME%
 if %ERRORLEVEL% neq 0 exit %ERRORLEVEL%
-
-cd rust/sasktran2-py-ext/ && cargo-bundle-licenses --format yaml --output ../../THIRDPARTY.yml
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,2 +1,2 @@
-$PYTHON -m pip install . -vv
 cd rust/sasktran2-py-ext/ && cargo bundle-licenses --format yaml --output ../../THIRDPARTY.yml
+$PYTHON -m pip install . -vv
