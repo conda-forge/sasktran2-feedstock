a01c3dd47de6b3d17e471d4df96acd2a9645f30e
Daniel Zawada <daniel.zawada@usask.ca>
2026-01-26 10:46:29 -0600

    Try to fix aarch64 cross compile

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,2 +1,11 @@
+# This allows the aarch64 linux build to cross compile correctly for cargo build tools?
+export CC_x86_64_unknown_linux_gnu="${CC_FOR_BUILD}"
+export CXX_x86_64_unknown_linux_gnu="${CXX_FOR_BUILD}"
+export CC_aarch64_unknown_linux_gnu="${CC}"
+export CXX_aarch64_unknown_linux_gnu="${CXX}"
+export HOST_CC="${CC_FOR_BUILD}"
+export HOST_CXX="${CXX_FOR_BUILD}"
+
+
 $PYTHON -m pip install . -vv
 cd rust/sasktran2-py-ext/ && cargo bundle-licenses --format yaml --output ../../THIRDPARTY.yml
