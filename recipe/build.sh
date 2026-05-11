611d20655467f8ddaa0efe7d4b0bbced94e82e23
Daniel Zawada <daniel.zawada@usask.ca>
2026-05-11 06:39:57 -0600

    Try to force link args on osx build

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,11 @@ export CXX_aarch64_unknown_linux_gnu="${CXX}"
 export HOST_CC="${CC_FOR_BUILD}"
 export HOST_CXX="${CXX_FOR_BUILD}"
 
+# This in the src cargo.toml, but force it here?
+if [[ "${target_platform}" == osx-* ]]; then
+  export RUSTFLAGS="-C link-arg=-undefined -C link-arg=dynamic_lookup ${RUSTFLAGS:-}"
+fi
+
 
 $PYTHON -m pip install . -vv
 cd rust/sasktran2-py-ext/ && cargo bundle-licenses --format yaml --output ../../THIRDPARTY.yml
